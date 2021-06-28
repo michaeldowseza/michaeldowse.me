9cf6a566ee20ff65f42555d2a8e034f709e53902
Michael Dowse <michael.dowse@live.com>
2021-06-28 07:22:09 +0200

    chore: Update dependencies
    - Update all old dependencies with security risks
    - Update in usage of CopyWebpackPlugin API

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,44 @@ const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   module: {
-    rules: [{
-      test: /\.js$/,
-      exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader'
-      }
-    }, {
-      test: /\.html$/,
-      use: [{
-        loader: 'html-loader',
-        options: {
-          minimize: true
-        }
-      }]
-    }]
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              minimize: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-      favicon: 'src/favicon.ico'
+      template: './src/index.html',
+      filename: './index.html',
+      favicon: 'src/favicon.ico',
     }),
-    new CopyWebpackPlugin([{
-        from: 'src/images',
-        to: 'images'
-      },
-      {
-        from: 'src/manifest.json',
-        to: 'manifest.json'
-      }
-    ]),
-  ]
-}
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: 'src/images',
+          to: 'images',
+        },
+        {
+          from: 'src/manifest.json',
+          to: 'manifest.json',
+        },
+      ],
+    }),
+  ],
+};
